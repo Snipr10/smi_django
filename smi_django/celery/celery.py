3ebb55fd216d639a7a7981d2a26e38326ca5a111
oleg <gusevoleg@gmail.com>
2021-08-30 16:41:03 +0300

    fix

--- smi_django/celery/celery.py
+++ smi_django/celery/celery.py
@@ -18,17 +18,18 @@ app.conf.beat_schedule = {
     #     'task': 'smi_django.tasks.start_task_parsing_echo',
     #     'schedule': crontab(minute='5, 35')
     # },
-
-    'start_task_parsing_radio': {
-        'task': 'smi_django.tasks.start_task_parsing_radio',
-        # 'schedule': crontab(minute='10, 40')
+    #
+    # 'start_task_parsing_radio': {
+    #     'task': 'smi_django.tasks.start_task_parsing_radio',
+    #     'schedule': crontab(minute='10, 40')
+    #
+    # },
+    'start_task_parsing_zenit': {
+        'task': 'smi_django.tasks.start_task_parsing_zenit',
+        #'schedule': crontab(minute='15, 45')
         'schedule': crontab(minute='*/1')
 
     },
-    # 'start_task_parsing_zenit': {
-    #     'task': 'smi_django.tasks.start_task_parsing_zenit',
-    #     'schedule': crontab(minute='15, 45')
-    # },
     # 'start_task_parsing_radiosvodoba': {
     #     'task': 'smi_django.tasks.start_task_parsing_radiosvodoba',
     #     'schedule': crontab(minute='20, 50')
