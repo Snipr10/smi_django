9480f7c70fc625ed036c1b0311d615dbb8225f12
oleg <gusevoleg@gmail.com>
2021-09-22 22:23:29 +0300

    add 5 tv

--- core/sites/5-tv.py
+++ core/sites/5-tv.py
@@ -1,20 +1,22 @@
 from datetime import datetime
 
+import dateparser
 import requests
 from bs4 import BeautifulSoup
 
 from core.sites.utils import update_proxy, DEFAULTS_TIMEOUT
 
 
+
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
-SEARCH_PAGE_URL = "https://www.radiorus.ru/search/json"
-PAGE_URL = "https://www.radiorus.ru/"
+SEARCH_PAGE_URL = "https://www.5-tv.ru/search/"
+PAGE_URL = ""
 
 
 # logger = get_logger(__name__)
 
 
-def parsing_radio_rus(keyword, limit_date, proxy, body):
+def parsing_5_tv(keyword, limit_date, proxy, body):
     is_not_stopped, body, is_time, proxy = get_urls(keyword, limit_date, proxy, [], 0)
     articles = []
     for article in body:
@@ -31,7 +33,7 @@ def get_page(articles, article_body, proxy):
     sounds = []
     videos = []
     try:
-        res = requests.get(PAGE_URL + article_body['href'], headers={
+        res = requests.get(article_body['href'], headers={
             "user-agent": USER_AGENT
         },
             proxies=proxy.get(list(proxy.keys())[0]),
@@ -39,21 +41,32 @@ def get_page(articles, article_body, proxy):
         )
         if res.ok:
             soup = BeautifulSoup(res.text)
-            text = soup.find("p", {"class": "anons"}).text
-            text_body = soup.find("p", {"align": "justify"})
+            container = _get_class_container(soup)
+            title = container.find("h1", {"class": "fsHeader1Alt"}).text
+            text = container.find("p", {"class": "fsHeaderAlt"}).text
+            p_text_body = container.find("div", {"class": "marginRightCol fsBig"}).find_all("p")
 
-            if text_body is not None:
-                text += "\n" + text_body.text
+            for text_body in p_text_body:
+                if "subscript" not in text_body.attrs.get('class', []):
+                    text += "\n" + text_body.text
 
             try:
-                for img in soup.find("div", {"class": "brand-episode__body"}).find_all("img"):
+                for img in container.find_all("img", {"class": "displayBlock"}):
                     try:
                         photos.append(img.attrs["src"])
                     except Exception:
                         pass
             except Exception:
                 pass
-            articles.append({"date": article_body['date'], "title": article_body['title'], "text": text.replace(" ", " ").strip(),
+            try:
+                for video in container.find_all("video"):
+                    try:
+                        videos.append(video.attrs["poster"])
+                    except Exception:
+                        pass
+            except Exception:
+                pass
+            articles.append({"date": article_body['date'], "title": title, "text": text.strip(),
                              "href": PAGE_URL + article_body['href'],
                              "photos": photos,
                              "sounds": sounds,
@@ -72,7 +85,7 @@ def get_urls(keyword, limit_date, proxy, body, page, attempts=0):
                            headers={
                                "user-agent": USER_AGENT
                            },
-                           params={"type": "episodes", "offset": page, "q": keyword},
+                           params={"n": 1, "q": keyword, "sort": "date", "page": page},
                            proxies=proxy.get(list(proxy.keys())[0]),
                            timeout=DEFAULTS_TIMEOUT
                            )
@@ -83,21 +96,21 @@ def get_urls(keyword, limit_date, proxy, body, page, attempts=0):
             return get_urls(keyword, limit_date, update_proxy(proxy), body, page, attempts + 1)
         return False, body, False, proxy
     if res.ok:
-        data = res.json().get("data", [])
-        if len(data) == 0:
+        soup = BeautifulSoup(res.text)
+        article_col3 = _get_class_container(soup).find_all("div", {"class": "col3"})
+        if len(article_col3) == 0:
             return True, body, False, proxy
-        for article in res.json().get("data", []):
-            article_date = datetime.strptime(article.get("date"), "%d-%m-%Y %H:%M:%S")
-            # if article_date.date() < limit_date.date():
-            #     return True, body, True, proxy
+        for article in article_col3:
+            article_date = dateparser.parse(article.text.strip())
+            if article_date.date() < limit_date.date():
+                return True, body, True, proxy
             if page > 100:
                 return True, body, True, proxy
             if article_date.date() >= limit_date.date():
                 body.append(
                     {
-                        "href": f"brand/{article.get('brands')[0].get('id')}/episode/{article.get('id')}",
+                        "href": article.find("a").attrs.get("href"),
                         "date": article_date,
-                        "title": article.get("title")
                     }
                 )
         return get_urls(keyword, limit_date, update_proxy(proxy), body, page + 1, attempts)
@@ -106,6 +119,10 @@ def get_urls(keyword, limit_date, proxy, body, page, attempts=0):
     return False, body, False, proxy
 
 
+def _get_class_container(soup):
+    return soup.find("main", {"class": "main"}).find("div", {"class": "container"})
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    parsing_radio_rus("spb", datetime.strptime("01/01/2000", "%d/%m/%Y"), None, [])
+    parsing_5_tv("spb", datetime.strptime("01/01/2000", "%d/%m/%Y"), None, [])
