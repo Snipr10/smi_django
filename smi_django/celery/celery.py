8c339dedaeead31d5917abc998f7fd975a99674c
oleg <gusevoleg@gmail.com>
2021-10-02 12:05:02 +0300

    test

--- smi_django/celery/celery.py
+++ smi_django/celery/celery.py
@@ -13,22 +13,26 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
-
-    'start_task_parsing_echo': {
-        'task': 'smi_django.tasks.start_task_parsing_echo',
-        'schedule': crontab(minute='5, 35')
+    'start_task_parsing_by_time': {
+        'task': 'smi_django.tasks.start_task_parsing_by_time',
+        # 'schedule': crontab(minute='5, 35')
+        'schedule': crontab(minute='*/5')
     },
-    'start_task_parsing_radio': {
-        'task': 'smi_django.tasks.start_task_parsing_radio',
-        'schedule': crontab(minute='10, 40')
-    },
-    'start_task_parsing_zenit': {
-        'task': 'smi_django.tasks.start_task_parsing_zenit',
-        'schedule': crontab(minute='15, 45')
-    },
-    'start_task_parsing_radiosvodoba': {
-        'task': 'smi_django.tasks.start_task_parsing_radiosvodoba',
-        'schedule': crontab(minute='20, 50')
-
-    },
+    # 'start_task_parsing_echo': {
+    #     'task': 'smi_django.tasks.start_task_parsing_echo',
+    #     'schedule': crontab(minute='5, 35')
+    # },
+    # 'start_task_parsing_radio': {
+    #     'task': 'smi_django.tasks.start_task_parsing_radio',
+    #     'schedule': crontab(minute='10, 40')
+    # },
+    # 'start_task_parsing_zenit': {
+    #     'task': 'smi_django.tasks.start_task_parsing_zenit',
+    #     'schedule': crontab(minute='15, 45')
+    # },
+    # 'start_task_parsing_radiosvodoba': {
+    #     'task': 'smi_django.tasks.start_task_parsing_radiosvodoba',
+    #     'schedule': crontab(minute='20, 50')
+    #
+    # },
 }
--- smi_django/tasks.py
+++ smi_django/tasks.py
@@ -1,6 +1,10 @@
+import datetime
+from django.utils import timezone
+
+from core.models import GlobalSite
 from smi_django.celery.celery import app
 
-from core.sites.utils import get_late_date, update_proxy, stop_proxy, save_articles
+from core.sites.utils import get_late_date, update_proxy, stop_proxy, save_articles, update_time_timezone
 from core.sites.echo import parsing_radio_echo, RADIO_URL as ECHO_RADIO_URL
 from core.sites.radio import parsing_radio, RADIO_URL
 from core.sites.radiozenit import parsing_radio_zenit, RADIO_URL as ZENIT_RADIO_URL
@@ -8,28 +12,48 @@ from core.sites.svoboda import parsing_radiosvoboda, RADIO_URL as SVOBODA_RADIO_
 
 
 @app.task
-def start_task_parsing_echo():
-    articles, proxy = parsing_radio_echo(get_late_date(ECHO_RADIO_URL), update_proxy(None))
-    stop_proxy(proxy)
-    save_articles(ECHO_RADIO_URL, articles)
+def start_task_parsing_by_time():
+    for site in GlobalSite.objects.filter(is_keyword=0, last_parsing__gte=update_time_timezone(
+            timezone.localtime()
+    ) - datetime.timedelta(minutes=5)):
+        try:
+            if site.url == RADIO_URL:
+                articles, proxy = parsing_radio(site.last_parsing, update_proxy(None))
+            if site.url == RADIO_URL:
+                articles, proxy = parsing_radio(site.last_parsing, update_proxy(None))
+            save_articles(RADIO_URL, articles)
+            site.last_parsing = update_time_timezone(timezone.localtime())
+            site.save(update_fields=["last_parsing"])
+        except Exception as e:
+            print(e)
 
+    # articles, proxy = parsing_radio_echo(get_late_date(ECHO_RADIO_URL), update_proxy(None))
+    # stop_proxy(proxy)
+    # save_articles(ECHO_RADIO_URL, articles)
 
-@app.task
-def start_task_parsing_radio():
-    articles, proxy = parsing_radio(get_late_date(RADIO_URL), update_proxy(None))
-    stop_proxy(proxy)
-    save_articles(RADIO_URL, articles)
-
-
-@app.task
-def start_task_parsing_zenit():
-    articles, proxy = parsing_radio_zenit(get_late_date(ZENIT_RADIO_URL), update_proxy(None))
-    stop_proxy(proxy)
-    save_articles(ZENIT_RADIO_URL, articles)
-
-
-@app.task
-def start_task_parsing_radiosvodoba():
-    articles, proxy = parsing_radiosvoboda(get_late_date(SVOBODA_RADIO_URL), update_proxy(None))
-    stop_proxy(proxy)
-    save_articles(SVOBODA_RADIO_URL, articles)
+# @app.task
+# def start_task_parsing_echo():
+#     articles, proxy = parsing_radio_echo(get_late_date(ECHO_RADIO_URL), update_proxy(None))
+#     stop_proxy(proxy)
+#     save_articles(ECHO_RADIO_URL, articles)
+#
+#
+# @app.task
+# def start_task_parsing_radio():
+#     articles, proxy = parsing_radio(get_late_date(RADIO_URL), update_proxy(None))
+#     stop_proxy(proxy)
+#     save_articles(RADIO_URL, articles)
+#
+#
+# @app.task
+# def start_task_parsing_zenit():
+#     articles, proxy = parsing_radio_zenit(get_late_date(ZENIT_RADIO_URL), update_proxy(None))
+#     stop_proxy(proxy)
+#     save_articles(ZENIT_RADIO_URL, articles)
+#
+#
+# @app.task
+# def start_task_parsing_radiosvodoba():
+#     articles, proxy = parsing_radiosvoboda(get_late_date(SVOBODA_RADIO_URL), update_proxy(None))
+#     stop_proxy(proxy)
+#     save_articles(SVOBODA_RADIO_URL, articles)
