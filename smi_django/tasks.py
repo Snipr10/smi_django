a71e5733b0a64d13a3ac93873480efad04761c37
oleg <gusevoleg@gmail.com>
2021-10-04 18:24:51 +0300

    test

--- smi_django/tasks.py
+++ smi_django/tasks.py
@@ -67,7 +67,9 @@ def add_new_key():
         new_keys = Keyword.objects.filter(~Q(id__in=keywords_list), network_id=1, disabled=0, enabled=1)
         for new_key in new_keys:
             new_key_list.append(SiteKeyword(site_id=site.site_id, keyword_id=new_key.id))
-            print(i)
+
+            if i % 1000:
+                print(i)
             i += 1
     SiteKeyword.objects.bulk_create(new_key_list, batch_size=200, ignore_conflicts=True)
 
