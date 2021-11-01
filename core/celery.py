8bf674afae0700f0c3e58b6d81c9ed0aac164f17
oleg <gusevoleg@gmail.com>
2021-11-01 15:26:05 +0300

    add br

--- core/celery.py
+++ core/celery.py
@@ -44,7 +44,7 @@ app.conf.beat_schedule = {
 
     'update_dp': {
         'task': 'smi_django.tasks.update_dp',
-        'schedule': crontab(minute='12')
+        'schedule': crontab(minute='30')
         # 'schedule': crontab(minute='*/12')
     },
 
