f99a1d6e61b07146edac57b5554e84d6d9534a6a
oleg <gusevoleg@gmail.com>
2021-12-02 09:41:09 +0300

    test

--- main.py
+++ main.py
@@ -71,8 +71,8 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     treads = []
-    for i in range(150):
-        treads.append(Thread(target=create_rmq, args=(i*100,)))
+    for i in range(10):
+        treads.append(Thread(target=create_rmq, args=(i*12123,)))
     for t in treads:
         t.start()
     for t in treads:
