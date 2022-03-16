3976ea128b3881e67f42d1e36b0b109e01463780
oleg <gusevoleg@gmail.com>
2022-03-16 09:31:29 +0300

    fix

--- core/sites/gov_spb/pars_gov.py
+++ core/sites/gov_spb/pars_gov.py
@@ -10,45 +10,85 @@ from core.sites.gov_spb.governor_disproof import parsing_governor_disproof
 
 def start_parsing(limit_date, proxy):
     all_articles = []
-    is_ok, articles, proxy = parsing_gov_government(limit_date, proxy)
-    if not is_ok:
-        print("can not parse parsing_gov_government")
-        raise Exception("can not parse parsing_gov_government")
-    all_articles.extend(articles)
+    max_limit = 100
 
-    is_ok, articles, proxy = parsing_gov_governor(limit_date, proxy)
-    if not is_ok:
-        print("can not parse parsing_gov_governor")
-        raise Exception("can not parse parsing_gov_governor")
-    all_articles.extend(articles)
+    limit = 0
+    while limit < max_limit:
+        is_ok, articles, proxy = parsing_gov_government(limit_date, proxy)
+        if is_ok:
+            all_articles.extend(articles)
+            limit = 2 * max_limit
+        else:
+            limit += 1
+            if limit >= max_limit:
+                print("can not parse parsing_gov_government")
+                raise Exception("can not parse parsing_gov_government")
 
-    is_ok, articles, proxy = parsing_gov_government_meeting(limit_date, proxy)
-    if not is_ok:
-        print("can not parse parsing_gov_government_meeting")
-        raise Exception("can not parse parsing_gov_government_meeting")
+    limit = 0
+    while limit < max_limit:
+        is_ok, articles, proxy = parsing_gov_governor(limit_date, proxy)
 
-    all_articles.extend(articles)
+        if is_ok:
+            all_articles.extend(articles)
+            limit = 2 * max_limit
+        else:
+            limit += 1
+            if limit >= max_limit:
+                print("can not parse parsing_gov_governor")
+                raise Exception("can not parse parsing_gov_governor")
 
-    is_ok, articles, proxy = parsing_gov_agency(limit_date, proxy)
-    if not is_ok:
-        print("can not parse parsing_gov_agency")
-        raise Exception("can not parse parsing_gov_agency")
+    limit = 0
+    while limit < max_limit:
+        is_ok, articles, proxy = parsing_gov_government_meeting(limit_date, proxy)
 
-    all_articles.extend(articles)
+        if is_ok:
+            all_articles.extend(articles)
+            limit = 2 * max_limit
+        else:
+            limit += 1
+            if limit >= max_limit:
+                print("can not parse parsing_gov_government_meeting")
+                raise Exception("can not parse parsing_gov_government_meeting")
 
-    is_ok, articles, proxy = parsing_gov_announces(limit_date, proxy)
-    if not is_ok:
-        print("can not parse parsing_gov_announces")
-        raise Exception("can not parse parsing_gov_announces")
+    limit = 0
+    while limit < max_limit:
+        is_ok, articles, proxy = parsing_gov_agency(limit_date, proxy)
 
-    all_articles.extend(articles)
+        if is_ok:
+            all_articles.extend(articles)
+            limit = 2 * max_limit
+        else:
+            limit += 1
+            if limit >= max_limit:
+                print("can not parse parsing_gov_agency")
+                raise Exception("can not parse parsing_gov_agency")
 
-    is_ok, articles, proxy = parsing_governor_disproof(limit_date, proxy)
-    if not is_ok:
-        print("can not parse parsing_governor_disproof")
-        raise Exception("can not parse parsing_governor_disproof")
+    limit = 0
+    while limit < max_limit:
+        is_ok, articles, proxy = parsing_gov_announces(limit_date, proxy)
 
-    all_articles.extend(articles)
+        if is_ok:
+            all_articles.extend(articles)
+            limit = 2 * max_limit
+        else:
+            limit += 1
+            if limit >= max_limit:
+                print("can not parse parsing_gov_announces")
+                raise Exception("can not parse parsing_gov_announces")
+
+    limit = 0
+    while limit < max_limit:
+        is_ok, articles, proxy = parsing_governor_disproof(limit_date, proxy)
+
+        if is_ok:
+            all_articles.extend(articles)
+            limit = 2 * max_limit
+        else:
+            limit += 1
+            if limit >= max_limit:
+                print("can not parse parsing_governor_disproof")
+                raise Exception("can not parse parsing_governor_disproof")
+
     print("gov parsing stop")
     return all_articles, proxy
 
