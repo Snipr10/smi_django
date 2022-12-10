b77b522a83effad4d5c296fb41bb9f79330bac6c
oleg.gusev <gusevoleg@gmail.com>
2022-12-10 11:03:07 +0300

    fixed mr7

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ CHANNELS = {}
 
 
 def open_save_chanel(i):
+    print(f"open_save_chanel 1")
     parameters_save = pika.URLParameters("amqp://full_posts_parser:nJ6A07XT5PgY@192.168.5.46:5672/smi_tasks")
+    print(f"open_save_chanel 2")
     connection_save = pika.BlockingConnection(parameters=parameters_save)
+    print(f"open_save_chanel 3")
     channel_save = connection_save.channel(channel_number=i * 20)
     print("open_save_chanel " + str(i))
     return channel_save
