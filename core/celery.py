dbce91b87f78aeec9474003d514595fdc5b7c326
oleg.gusev <oleg.gusev@gmail.com>
2023-10-22 10:42:51 +0300

    test

--- core/celery.py
+++ core/celery.py
@@ -16,21 +16,21 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
+    # #
+    # # 'rabbit_mq': {
+    # #     'task': 'smi_django.tasks.rabbit_mq',
+    # #     'schedule': crontab(minute='*/1')
+    # # },
     #
-    # 'rabbit_mq': {
-    #     'task': 'smi_django.tasks.rabbit_mq',
-    #     'schedule': crontab(minute='*/1')
+    # # 'start_task_parsing_by_time': {
+    # #     'task': 'smi_django.tasks.start_task_parsing_by_time',
+    # #     'schedule': crontab(minute='*/10')
+    # # },
+    # 'add_new_key': {
+    #     'task': 'smi_django.tasks.add_new_key',
+    #     # 'schedule': crontab(minute='29')
+    #     'schedule': crontab(minute='*/15')
     # },
-
-    # 'start_task_parsing_by_time': {
-    #     'task': 'smi_django.tasks.start_task_parsing_by_time',
-    #     'schedule': crontab(minute='*/10')
-    # },
-    'add_new_key': {
-        'task': 'smi_django.tasks.add_new_key',
-        # 'schedule': crontab(minute='29')
-        'schedule': crontab(minute='*/15')
-    },
     'task_parsing_key': {
         'task': 'smi_django.tasks.task_parsing_key',
         # 'schedule': crontab(minute='5, 35')
@@ -38,62 +38,62 @@ app.conf.beat_schedule = {
         # 'schedule': crontab(minute='*/1')
 
     },
-    'untaken_key': {
-        'task': 'smi_django.tasks.untaken_key',
-        'schedule': crontab(minute='*/30')
-        # 'schedule': crontab(minute='*/12')
-    },
-    'update_key_pool': {
-        'task': 'smi_django.tasks.update_key_pool',
-        'schedule': crontab(minute='*/5')
-        # 'schedule': crontab(minute='*/12')
-    }
-    # 'update_smi': {
-    #     'task': 'smi_django.tasks.update_smi',
-    #     # 'schedule': crontab(minute='*/1')
-    #     'schedule': 2.0,
-    #
+    # 'untaken_key': {
+    #     'task': 'smi_django.tasks.untaken_key',
+    #     'schedule': crontab(minute='*/30')
     #     # 'schedule': crontab(minute='*/12')
     # },
-    # 'update_smi_new': {
-    #     'task': 'smi_django.tasks.update_smi_new',
-    #     'schedule': crontab(minute='*/1')
-    #     # 'schedule': 2.0,
-    #
+    # 'update_key_pool': {
+    #     'task': 'smi_django.tasks.update_key_pool',
+    #     'schedule': crontab(minute='*/5')
     #     # 'schedule': crontab(minute='*/12')
-    # },
-
-    #
-    #
-    # 'update_dp': {
-    #     'task': 'smi_django.tasks.update_dp',
-    #     'schedule': crontab(minute='30')
-    #     # 'schedule': crontab(minute='*/12')
-    # },
-
-    # 'update_time': {
-    #     'task': 'smi_django.tasks.update_time',
-    #      'schedule': crontab(minute='57')
     # }
-    # 'delete_bad_posts': {
-    #     'task': 'smi_django.tasks.delete_bad_posts',
-    #      'schedule': crontab(minute='30')
-    # }
-    # 'start_task_parsing_echo': {
-    #     'task': 'smi_django.tasks.start_task_parsing_echo',
-    #     'schedule': crontab(minute='5, 35')
-    # },
-    # 'start_task_parsing_radio': {
-    #     'task': 'smi_django.tasks.start_task_parsing_radio',
-    #     'schedule': crontab(minute='10, 40')
-    # },
-    # 'start_task_parsing_zenit': {
-    #     'task': 'smi_django.tasks.start_task_parsing_zenit',
-    #     'schedule': crontab(minute='15, 45')
-    # },
-    # 'start_task_parsing_radiosvodoba': {
-    #     'task': 'smi_django.tasks.start_task_parsing_radiosvodoba',
-    #     'schedule': crontab(minute='20, 50')
+    # # 'update_smi': {
+    # #     'task': 'smi_django.tasks.update_smi',
+    # #     # 'schedule': crontab(minute='*/1')
+    # #     'schedule': 2.0,
+    # #
+    # #     # 'schedule': crontab(minute='*/12')
+    # # },
+    # # 'update_smi_new': {
+    # #     'task': 'smi_django.tasks.update_smi_new',
+    # #     'schedule': crontab(minute='*/1')
+    # #     # 'schedule': 2.0,
+    # #
+    # #     # 'schedule': crontab(minute='*/12')
+    # # },
     #
-    # },
+    # #
+    # #
+    # # 'update_dp': {
+    # #     'task': 'smi_django.tasks.update_dp',
+    # #     'schedule': crontab(minute='30')
+    # #     # 'schedule': crontab(minute='*/12')
+    # # },
+    #
+    # # 'update_time': {
+    # #     'task': 'smi_django.tasks.update_time',
+    # #      'schedule': crontab(minute='57')
+    # # }
+    # # 'delete_bad_posts': {
+    # #     'task': 'smi_django.tasks.delete_bad_posts',
+    # #      'schedule': crontab(minute='30')
+    # # }
+    # # 'start_task_parsing_echo': {
+    # #     'task': 'smi_django.tasks.start_task_parsing_echo',
+    # #     'schedule': crontab(minute='5, 35')
+    # # },
+    # # 'start_task_parsing_radio': {
+    # #     'task': 'smi_django.tasks.start_task_parsing_radio',
+    # #     'schedule': crontab(minute='10, 40')
+    # # },
+    # # 'start_task_parsing_zenit': {
+    # #     'task': 'smi_django.tasks.start_task_parsing_zenit',
+    # #     'schedule': crontab(minute='15, 45')
+    # # },
+    # # 'start_task_parsing_radiosvodoba': {
+    # #     'task': 'smi_django.tasks.start_task_parsing_radiosvodoba',
+    # #     'schedule': crontab(minute='20, 50')
+    # #
+    # # },
 }
